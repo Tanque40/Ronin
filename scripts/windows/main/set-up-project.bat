7a4a22bf95fb1748aeab934f12f17b8ce7b4bc10
Bruno Vitte <bruno_vitte@hotmail.com>
2025-07-07 23:27:53 -0600

    move bats files

--- scripts/windows/main/set-up-project.bat
+++
@@ -1,4 +0,0 @@
-:: Setup ronin project with premake
-.\vendor\repos\premake\bin\release\premake5.exe vs2022
-
-PAUSE
--- set-up-premake.bat
+++ set-up-premake.bat
@@ -1,7 +1,5 @@
 :: Setup premake
 
-:: go to the root of the project
-cd ../../../
 :: create a premake install
 cd vendor/repos/premake
 Bootstrap.bat
